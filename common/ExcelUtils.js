ba0b5075228f2a424e4858e8ae1d9a441310b967
menglonghua <2322931413@qq.com>
2023-05-05 23:09:15 +0800

    mlh

--- common/ExcelUtils.js
+++ common/ExcelUtils.js
@@ -3,33 +3,33 @@
  * @param {Object} int pageNo 页码
  * @param {Object} int pageSize 大小
  */
-export function read(int pageNo,int pageSize){
-	// 分页默认前十行
-	pageNo = pageNo == null || pageNo === 0 ? 1 : pageNo;
-	pageSize = pageSize == null || pageSize === 0 ? 10 : pageSize;
+// export function read(int pageNo,int pageSize){
+// 	// 分页默认前十行
+// 	pageNo = pageNo == null || pageNo === 0 ? 1 : pageNo;
+// 	pageSize = pageSize == null || pageSize === 0 ? 10 : pageSize;
 	
-	// 分页查询Excel的行数
-	pageNo = pageNo * pageSize;
-	pageSize = pageNo + pageSize;
+// 	// 分页查询Excel的行数
+// 	pageNo = pageNo * pageSize;
+// 	pageSize = pageNo + pageSize;
 	
-	// 导入SheetJS库
-	const XLSX = require('xlsx');
+// 	// 导入SheetJS库
+// 	const XLSX = require('xlsx');
 	
-	// 读取Excel文件 默认是相对路径
-	const workbook = XLSX.readFile('../static/data/cus_info.xlsx');
+// 	// 读取Excel文件 默认是相对路径
+// 	const workbook = XLSX.readFile('../static/data/cus_info.xlsx');
 	
-	// 获取第一个工作表
-	const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+// 	// 获取第一个工作表
+// 	const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 	
-	/ 指定要读取的行数
-	const range = {s: {r: pageNo}, e: {r: pageSize}}; // 读取前10行
+// 	/ 指定要读取的行数
+// 	const range = {s: {r: pageNo}, e: {r: pageSize}}; // 读取前10行
 
-	// 将工作表转换为JSON对象
-	const data = XLSX.utils.sheet_to_json(worksheet, {header:1, range});
+// 	// 将工作表转换为JSON对象
+// 	const data = XLSX.utils.sheet_to_json(worksheet, {header:1, range});
 	
-	// 输出结果
-	console.log(data);
+// 	// 输出结果
+// 	console.log(data);
 	
-	return data;
-}
+// 	return data;
+// }
 
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,14 +11,37 @@
 	export default {
 		data() {
 			return {
-				title: 'index1'
+				title: 'index1',
 			}
 		},
 		onLoad() {
-
+		// 	let data = [
+		// 		['村组名', '运营商', '消费/月','是否办理融合', '融合到期时间', '年龄段','家庭成员', '成员总共消费', '网络情况','联系电话', '用网习惯', '是否意向用户']
+		// 	];
+			
+		// 	// 获取static目录的绝对路径
+		// 	let staticPath = uni.getStorageSync('static');
+		
+		// 	// 拼接Excel文件的路径
+		// 	let filePath = `${staticPath}/data.xlsx`;
+		
+		// 	// 创建一个XlsExport对象，并设置文件名和工作表名称
+		// 	let xls = new XlsExport(data, 'data.xlsx', 'Sheet1');
+		
+		// 	// 将数据导出为Excel文件，并使用uni.saveFile方法将文件保存到用户手机中
+		// 	uni.saveFile({
+		// 		tempFilePath: xls.base64(),
+		// 		filePath: filePath,
+		// 		success: function (res) {
+		// 		  console.log('Excel文件已保存到用户手机中');
+		// 		},
+		// 		fail: function (res) {
+		// 		  console.log('保存Excel文件失败:', res);
+		// 		}
+		// 	});
 		},
 		methods: {
-
+			
 		}
 	}
 </script>
