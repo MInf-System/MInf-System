72fb768a991810a817a71a0f34c2164d2accbee0
jiawei <1250924837@qq.com>
2023-05-07 23:19:43 +0800

    小伟
    文件追加更新

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -57,7 +57,7 @@
 				<view class="title">网络情况</view>
 				<input class="uni-input" v-model="situation" />
 			</view>
-			<view class="uni-form-item uni-column">
+			<!-- <view class="uni-form-item uni-column">
 				<view class="title">联系电话</view>
 				<input class="uni-input" v-model="telephone" type="tel" @input="validatePhone" placeholder="电话号码" />
 				<div v-show="phoneInvalid" style="color: red;">请输入有效的手机号码</div>
@@ -69,7 +69,7 @@
 			<view class="uni-form-item uni-column">
 				<view class="title">是否意向用户</view>
 				<input class="uni-input" v-model="intention" />
-			</view>
+			</view> -->
 			<view class="uni-btn-v">
 				<button form-type="submit">Submit</button>
 				<button type="default" form-type="reset">Reset</button>
@@ -81,7 +81,9 @@
 <script>
 	// import XlsExport from 'xlsexport';
 	// import { createRequire } from 'module';
-	
+	// import uni from 'uni-app';
+	// import XLSX from 'xlsx';
+	// import uni from 'uni-app';
 	export default {
 		data() {
 			return {
@@ -112,27 +114,28 @@
 			},
 			formSubmit: function(e) {
 				//获取表单数据
-				const data = {
-					villageName: this.villageName,
-					operator: this.operator,
-					consumption: this.consumption,
-					handle: this.handle,
-					maturityTime: this.maturityTime,
-					age: this.age,
-					number: this.number,
-					allConsumption: this.allConsumption,
-					situation: this.situation,
-					telephone: this.telephone,
-					habit: this.habit,
-					intention: this.intention
-				}
+				const data = [{
+						villageName: this.villageName,
+						operator: this.operator,
+						consumption: this.consumption,
+						handle: this.handle,
+						maturityTime: this.maturityTime,
+						age: this.age,
+						number: this.number,
+						allConsumption: this.allConsumption,
+						situation: this.situation,
+						telephone: this.telephone,
+						habit: this.habit,
+						intention: this.intention
+					}
+				]
 				console.log('form发生了submit事件，携带数据为：' + data)
 				var formdata = data
-				this.saveExcel(JSON.stringify(data));
-				uni.showModal({
-					content: '表单数据内容：' + JSON.stringify(data),
-					showCancel: false
-				});
+				this.saveExcel(data);
+				// uni.showModal({
+				// 	content: '表单数据内容：' + JSON.stringify(data),
+				// 	showCancel: false
+				// });
 				
 				// let dataex = [
 				// 	['村组名', '运营商', '消费/月','是否办理融合', '融合到期时间', '年龄段','家庭成员', '成员总共消费', '网络情况','联系电话', '用网习惯', '是否意向用户']
@@ -164,25 +167,110 @@
 				console.log("***************保存Excel数据中。。。****************")
 				console.log("*数据=>",data);
 				
+				// 还未实现
+				// TODO 判断文件是否存在，如果不存在则创建空文件
 				const fs = require('fs');
+				console.log("加载fs工具");
+				const databaseFile = `${fs.rootPath}/data/data.xlsx`;
+				uni.requestFileSystem({
+				  // 指定文件系统的类型，这里使用临时文件系统
+				  // 如果要持久保存文件，则需要使用本地文件系统
+				  // 详情请参考文档：https://uniapp.dcloud.io/api/file/file-system?id=requestfilesystem
+				  type: 0,
+				  success(fs) {
+				    // 获取文件信息
+				    uni.getFileInfo({
+				      filePath: `${fs.rootPath}/data/data.xlsx`,
+				      success(res) {
+				        console.log('文件已存在')
+				      },
+				      fail(err) {
+				        console.log('文件不存在')
+				      }
+				    })
+				  }
+				})
 				
+				
+				const XLSX = require('xlsx');
 				console.log("加载Excel工具");
-				const ExcelJS = require('xlsx');
-				const workbook = new ExcelJS.Workbook();
 				
-				const databaseLocd = '@/data.xlsx';
-				workbook.xlsx.readFile(databaseLocd).then(() => {
-				    const worksheet = workbook.getWorksheet('data');
-					console.log('获取Excel工作表=>',worksheet)
-				    worksheet.addRow(data);
-					console.log('追加数据=>成功')
-				    return workbook.xlsx.writeFile(databaseLocd);
-				  }).then(() => {
-				    console.log('File updated!');
-				  }).catch((error) => {
-				    console.log('Error:', error);
-				    console.log('Error:', error.message);
-				  });
+				// const workbook = XLSX.readFile(databaseFile);
+				const workbook = XLSX.read(databaseFile, { type: 'binary' });
+				console.log("打开excel成功");
+				
+				// 获取第一个工作表
+				const sheetName = workbook.SheetNames[0];
+				const worksheet = workbook.Sheets[sheetName];
+				console.log("获取第一个工作表成功");
+				
+				// 计算新数据的行数和列数
+				const newRows = XLSX.utils.json_to_sheet(data);
+				const range = XLSX.utils.decode_range(newRows['!ref']);
+				const rowCount = range.e.r - range.s.r + 1;
+				const colCount = range.e.c - range.s.c + 1;
+				console.log("计算新数据的行数和列数rowCount:",rowCount,"colCount",colCount);
+				
+				// 将新数据添加到工作表中
+				XLSX.utils.sheet_add_json(worksheet, data, {
+				  origin: -1, // 从最后一行开始追加数据
+				  skipHeader: true, // 不要在工作表中添加标题行
+				  header: ['name', 'age'], // 指定标题行的字段名
+				  originRow: worksheet['!ref'] ? range.e.r + 1 : 0, // 从第一行开始追加数据
+				});
+				console.log("将新数据添加到工作表中");
+				
+				// 更新工作表的范围
+				worksheet['!ref'] = XLSX.utils.encode_range({
+				  s: { c: 0, r: 0 }, // 左上角单元格的列号和行号
+				  e: { c: colCount - 1, r: rowCount + range.e.r }, // 右下角单元格的列号和行号
+				});
+				console.log("更新工作表的范围");
+				
+				// 一定需要excel文件存在
+				XLSX.writeFile(workbook, databaseFile);
+				
+				
+				// 将 workbook 转换成二进制数据
+				// const wbout = XLSX.write(workbook, {bookType:'xlsx', type:'array'});
+				
+				// 定义保存文件的文件名和文件类型
+				// 只能保存文件，不能新增
+				// uni.saveFile({
+				//   tempFilePath: '/static/data/data.xlsx',
+				//   filePath: '/static/data/data.xlsx',
+				//   success: function(res) {
+				//     // 保存成功
+				//     console.log('saveFile success', res);
+				//     console.log('saveFile success', res.savedFilePath);
+				//   },
+				//   fail: function(err) {
+				//     // 保存失败
+				//     console.log('saveFile fail', err);
+				//   },
+				//   complete: function() {
+				//     // 保存完成
+				//     console.log('saveFile complete');
+				//   },
+				//   data: wbout
+				// });
+				
+				
+				console.log("保存excel");
+				
+				// const databaseLocd = '@/data.xlsx';
+				// workbook.xlsx.readFile(databaseLocd).then(() => {
+				//     const worksheet = workbook.getWorksheet('data');
+				// 	console.log('获取Excel工作表=>',worksheet)
+				//     worksheet.addRow(data);
+				// 	console.log('追加数据=>成功')
+				//     return workbook.xlsx.writeFile(databaseLocd);
+				//   }).then(() => {
+				//     console.log('File updated!');
+				//   }).catch((error) => {
+				//     console.log('Error:', error);
+				//     console.log('Error:', error.message);
+				//   });
 
 				console.log("***************保存Excel数据---成功。。。****************")
 			},
